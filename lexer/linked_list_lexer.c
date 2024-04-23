74d32fb2948f845e077f4ca1c8dc7c1901b1c4a7
Mohamed Amine Boukour <mboukour@e2r4p8.1337.ma>
2024-04-23 17:55:50 +0100

    Lexer Done

--- lexer/linked_list_lexer.c
+++ lexer/linked_list_lexer.c
@@ -47,6 +47,19 @@ t_lex	*ft_lstfirst_lex(t_lex *lst)
 	return (lst);
 }
 
+int counter(t_lex *current, char c)
+{
+	int count;
+
+	count = 0;
+	while(current)
+	{
+		if(current->len == 1 && current->content[0] == c)
+			count++;
+		current = current->prev;
+	}
+	return (count);
+}
 
 
 void	ft_lstadd_back_lex(t_lex **lst, t_lex *new)
@@ -58,15 +71,24 @@ void	ft_lstadd_back_lex(t_lex **lst, t_lex *new)
 	if (!*lst)
 	{
 		*lst = new;
+		new->state = GENERAL;
 		return ;
 	}
 	last_lex = ft_lstlast_lex(*lst);
 	last_lex->next = new;
 	new->prev = last_lex;
+	if(new->prev && (new->prev->token == DOUBLE_QUOTE || new->prev->state == IN_DQUOTE) && (counter(new, '"') % 2)  && new->token != DOUBLE_QUOTE)
+		new->state = IN_DQUOTE;
+	else if (new->prev && (new->prev->token == QUOTE || new->prev->state == IN_QUOTE) && (counter(new, '\'') % 2) && new->token != QUOTE)
+		new->state = IN_QUOTE;
+	else
+		new->state = GENERAL;
 }
 
-const char* tokenToString(token t) {
-    switch (t) {
+const char* tokenToString(token t)
+{
+    switch (t) 
+	{
         case WORD: return "WORD";
         case WHITE_SPACE: return "WHITE_SPACE";
         case NEW_LINE: return "NEW_LINE";
@@ -83,17 +105,28 @@ const char* tokenToString(token t) {
     }
 }
 
+const char* stateToString(state s)
+{
+    switch (s) {
+        case IN_DQUOTE: return "IN_DQUOTE";
+        case IN_QUOTE: return "IN_QUOTE";
+        case GENERAL: return "GENERAL";
+        default: return "UNKNOWN_STATE";
+    }
+}
+
 
 void print_lex(const t_lex *lex) {
-    printf("Content: %s\n", lex->content);
-    printf("Length: %d\n", lex->len);
-	printf("Token: %s\n", tokenToString(lex->token));
-    // printf("State: %d\n", lex->state);
-	printf("----------------------\n");
+
+    printf("| %-10s | %-15s | %-10d | %-15s |\n", lex->content, tokenToString(lex->token), lex->len, stateToString(lex->state));
+    printf("| %-10s | %-15s | %-10s | %-15s |\n", "", "", "", "");
+    printf("|------------|-----------------|------------|-----------------|\n");
 }
 
 void ft_lstiter_lex(t_lex *lex)
 {
+    printf("| %-10s | %-15s | %-10s | %-15s |\n", "Content", "Token", "Length", "State");
+    printf("|------------|-----------------|------------|-----------------|\n");
 	while(lex)
 	{
 		print_lex(lex);
