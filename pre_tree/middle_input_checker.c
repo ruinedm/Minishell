5d393170eb7acd4649bf4e83ab68df633035082b
Mohamed Amine Boukour <mboukour@e1r7p14.1337.ma>
2024-05-22 10:49:48 +0100

    Error

--- pre_tree/middle_input_checker.c
+++ pre_tree/middle_input_checker.c
@@ -1,19 +1,25 @@
 #include "../minishell.h"
 
 
-
 t_middle *middle_input_checker(t_middle *middled)
 {
 	t_middle *next;
 
 	while(middled)
 	{
+		next = middled->next;
 		if(middled->token == OPEN_PARANTHESE)
 		{
-			next = middled->next;
 			if(!next)
 				return (middled);
 			else if(next->token == CLOSE_PARANTHESE)
+				return (next);
+		}
+		else if(middled->token == REDIR_IN || middled->token == REDIR_OUT || middled->token == DREDIR_OUT || middled->token == DREDIR_OUT)
+		{
+			if(!next)	
+				return (middled);
+			else if (next->token != COMMAND)
 				return (next);
 		}
 		middled = middled->next;
